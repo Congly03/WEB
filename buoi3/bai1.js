59df7b312b4c33b0535e727925b43e6ed509ed30
Congly03 <143502440+Congly03@users.noreply.github.com>
2024-04-01 20:46:24 +0700

    3

---
+++ buoi3/bai1.js
@@ -0,0 +1,24 @@
+var sachs = [
+    {
+        tacgia : 'Harper Lee',
+        tieuDe : 'To Kill a Mockingbird',
+        namXuatBan : 2020,
+        trangthai : true
+    },
+    {
+        tacgia : 'Ray Bradbury',
+        tieuDe : 'Fahrenheit 451',
+        namXuatBan :  2022,
+        trangthai : false
+    }
+]
+sachs.forEach(function(sachs) {
+    console.log(sachs.tacgia);
+    console.log(sachs.tieuDe);
+    console.log(sachs.namXuatBan);
+    console.log(sachs.trangthai);
+});
+
+
+
+
